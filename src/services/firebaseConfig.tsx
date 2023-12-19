3340831558521e2bf8073373ae07b3466d9276ff
Katiana Hanisch <katianahanisch@gmail.com>
2023-12-19 15:23:16 -0400

    ajustes na aplicacao

--- src/services/firebaseConfig.tsx
+++ src/services/firebaseConfig.tsx
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBb8odXKZB9BlgQDwiQ_Zn8l7YKGWyu-Ys",
-  authDomain: "n1-mobile.firebaseapp.com",
-  projectId: "n1-mobile",
-  storageBucket: "n1-mobile.appspot.com",
-  messagingSenderId: "377613745153",
-  appId: "1:377613745153:web:55ff2e2fbcd4feb8c91400",
-  measurementId: "G-0VT0NYF4EE",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const reactNativePersistence = (firebaseAuth as any).getReactNativePersistence;
